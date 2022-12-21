34b6c707de8d9ea126aeae9b667b96870b65644e
Ctucker9233 <tuckersfurniture@gmail.com>
2022-12-21 17:21:02 +0000

    added symfony asset dependency

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/HomeController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    //...
+
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig');
+    }
+}
