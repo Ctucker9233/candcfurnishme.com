8875db8a71715eafbf27af06085f073a213eff95
Ctucker9233 <tuckersfurniture@gmail.com>
2022-12-24 00:35:35 +0000

    added user table to database

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     *@Route ("/")
+     *@Route ("/", name="app_homepage")
      */
 
     public function index(): Response
